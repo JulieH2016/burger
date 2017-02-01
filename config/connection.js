2027071adf55f801f17e40d98fb0be53818bd1b1
Julie Hoffman <jhoffman2013@gmail.com>
2017-02-01 16:04:33 -0500

    Added connection.js file and code to connect node to mysql

--- config/connection.js
+++ config/connection.js
@@ -12,3 +12,5 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
 })
+
+// Connected Node to MySQL. Will fill in password and database name once I have them.
