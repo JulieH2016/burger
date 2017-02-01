655765a0f07c7682f38ed05b02e90b8705bbd2ab
Julie Hoffman <jhoffman2013@gmail.com>
2017-02-01 16:13:46 -0500

    Added server.js content to require 3 packages

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ var methodOverride = require("method-override");
 var app = express();
 var port = 3000;
 
+//Required 3 packages for this project - express, method-override and body-parser.
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
